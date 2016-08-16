84b2787d32f28ab89fcc564c24ac79cceaa4ed6a
nzer0 <justjest@gmail.com>
2016-08-16 22:48:45 +0900

    auto-initialization is implemented

--- h5block.py
+++ h5block.py
@@ -3,13 +3,17 @@ import inspect
 
 
 class _DsetWrap(object):
-    def __init__(self, file_descriptor, dset_descriptor):
+    def __init__(self, name, file_descriptor, dset_descriptor):
+        self.name = name
         self.fd = file_descriptor
         self.ds = dset_descriptor
         self.members = [m[0] for m in inspect.getmembers(self.ds)]
 
     def __str__(self):
-        return "<h5block "+self.ds.__str__()+", maxshape {}>".format(self.ds.maxshape)
+        if self.ds is None:
+            return "<h5block auto-created dataset>"
+        else:
+            return "<h5block "+self.ds.__str__()+", maxshape {}>".format(self.ds.maxshape)
 
     def __repr__(self):
         return self.__str__()
@@ -27,29 +31,42 @@ class _DsetWrap(object):
         return self.ds.__setitem__(key, value)
 
     def extend(self, data, axis=None):
-        maxshape = self.ds.maxshape
-        curr_shape = self.ds.shape
-        extd_shape = data.shape
-        if len(curr_shape) != len(extd_shape):
-            raise ValueError('Number of axes does not match with the original data')
+        if self.ds is None: # handling auto-init
+            if axis is None:
+                raise ValueError("Keyword arggument 'axis' has to be given for auto-initialization")
+            if not isinstance(axis, list):
+                axis = [axis]
+            maxshape = list(data.shape)
+            for i in axis:
+                maxshape[i] = None
 
-        if axis is None:
-            if maxshape.count(None) > 1:
-                raise ValueError('You need to specify the axis when there are multiple extendable axes')
-            axis = maxshape.index(None) # first extendable axis
+            self.ds = self.fd.create_dataset(self.name, data=data, maxshape=tuple(maxshape) )
+            self.fd.flush()
+        else:
 
-        for i in xrange(len(curr_shape)):
-            if i!=axis and curr_shape[i] != extd_shape[i]:
-                raise ValueError('Dimension {} does not match with the original data'.format(i))
-        
-        new_shape = list(curr_shape)
-        new_shape[axis] += extd_shape[axis]
-        self.ds.resize(tuple(new_shape))
+            maxshape = self.ds.maxshape
+            curr_shape = self.ds.shape
+            extd_shape = data.shape
+            if len(curr_shape) != len(extd_shape):
+                raise ValueError('Number of axes does not match with the original data')
 
-        slcs = [slice(None,None,None) for s in xrange(len(curr_shape))]
-        slcs[axis] = slice(curr_shape[axis], new_shape[axis], None)
-        self.ds[tuple(slcs)] = data
-        self.fd.flush()
+            if axis is None:
+                if maxshape.count(None) > 1:
+                    raise ValueError('You need to specify the axis when there are multiple extendable axes')
+                axis = maxshape.index(None) # first extendable axis
+
+            for i in xrange(len(curr_shape)):
+                if i!=axis and curr_shape[i] != extd_shape[i]:
+                    raise ValueError('Dimension {} does not match with the original data'.format(i))
+            
+            new_shape = list(curr_shape)
+            new_shape[axis] += extd_shape[axis]
+            self.ds.resize(tuple(new_shape))
+
+            slcs = [slice(None,None,None) for s in xrange(len(curr_shape))]
+            slcs[axis] = slice(curr_shape[axis], new_shape[axis], None)
+            self.ds[tuple(slcs)] = data
+            self.fd.flush()
 
 
 class _FileWrap(object):
@@ -65,6 +82,11 @@ class _FileWrap(object):
         return self.__str__()
 
     def __getitem__(self, key):
+        if not key in self.dic.keys():
+            dw = _DsetWrap(key, self.fd, None)
+            self.dic[key] = dw
+            import warnings
+            warnings.warn('Auto-creating the non-existing dataset')
         return self.dic[key]
 
     def __setitem__(self, key, value):
@@ -98,7 +120,7 @@ class _FileWrap(object):
 
         ds = self.fd.create_dataset(*args, **kwargs)
         self.fd.flush()
-        dw = _DsetWrap(self.fd, ds)
+        dw = _DsetWrap(args[0], self.fd, ds)
         self.dic[args[0]] = dw
         return dw
 
